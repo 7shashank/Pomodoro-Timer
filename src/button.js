86b7e13e5c25627a13f50c03d59f6837ac737e8e
7shashank <1234567890shashank@gmail.com>
2024-01-24 09:29:04 +0530

    Removed logs

--- src/button.js
+++ src/button.js
@@ -2,7 +2,7 @@ import React from 'react';
 // const button_click = new Audio('/button_click.wav');
 
 function Button(props) {
-  console.log(props);
+  // console.log(props);
   return (
     <button onClick={props.onClickFunc}>{props.buttonText}</button>
   )
