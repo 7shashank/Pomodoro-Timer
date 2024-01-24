ca8fef55e5524444dc8ea9d2ac061868faf99521
7shashank <1234567890shashank@gmail.com>
2024-01-24 10:06:26 +0530

    Added Button click sound

--- src/button.js
+++ src/button.js
@@ -1,10 +1,14 @@
 import React from 'react';
-// const button_click = new Audio('/button_click.wav');
 
 function Button(props) {
-  // console.log(props);
+  const playSound = () => {
+    const button_click = new Audio('/button_click.mp3');
+    button_click.volume = 0.65;
+    button_click.play();
+  }
+  // console.log(button_click);
   return (
-    <button onClick={props.onClickFunc}>{props.buttonText}</button>
+    <button onClick={()=>{playSound(); props.onClickFunc();}}>{props.buttonText}</button>
   )
 }
 
